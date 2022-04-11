ccc7e0fff8325e3d05dc2d2bfb65d816a37c2780
FelipeIot <sarche_2004@hotmail.com>
2022-04-10 21:41:26 -0500

    final antes de refactor

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,41 @@ const marcas = ['X', 'O'];
 var jugadores;
 var tablero;
 var turno;
+var numerojugadas;
+var ganador;
+
+
+const verificarGanador=(tablero)=>{ 
+  let resultado = false;
+  //verificar si existe un ganador en el tablero  (horizontal)
+  tablero.forEach(fila => {
+    if (fila[0] === fila[1] && fila[1] === fila[2] && fila[0] !== ' ') {
+      resultado = true;
+    }
+  });
+  //verificar si existe un ganador en el tablero  (diagonal)
+  if (tablero[0][0] === tablero[1][1] && tablero[1][1] === tablero[2][2] && tablero[0][0] !== ' ') {
+    resultado = true;
+  }
+  //verificar si existe un ganador en el tablero  (vertical)
+  for (let i = 0; i < 3; i++) {
+    if (tablero[0][i] === tablero[1][i] && tablero[1][i] === tablero[2][i] && tablero[0][i] !== ' ') {
+      resultado = true;
+    }
+  }
+  return resultado;
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* PUT empezar. */
 
+/* PUT empezar. */
 router.put('/empezar', function (req, res) {
   turno = 0;
+  ganador = 'ninguno';
+  numerojugadas = 0;
   jugadores = req.body.jugadores;
   tablero =
     [
@@ -24,21 +50,35 @@ router.put('/empezar', function (req, res) {
     ];
   res.setHeader('Content-Type', 'application/json');
   res.send({
-    turno: jugadores[turno], tablero: tablero
+    turno: jugadores[turno], tablero: tablero, ganador: ganador
   });
 });
 
 /* PUT movimiento. */
 router.put('/movimiento', function (req, res) {
+  numerojugadas++;
+  if (numerojugadas >= 9) {
+    ganador = 'empate';
+  }
+
+
+
   const columna = req.body.columna;
   const fila = req.body.fila;
-  tablero[fila][columna] = marcas[turno];
+  if (jugadores[turno] === req.body.jugador) {
+    tablero[fila][columna] = marcas[turno];
+    if (verificarGanador(tablero) === true) {
+      ganador = jugadores[turno];
+    }
 
-  turno = (turno + 1) % 2 ;
-  console.log(turno);
+    turno = (turno + 1) % 2;
+  }
+
+
+
   res.setHeader('Content-Type', 'application/json');
   res.send({
-    turno: jugadores[turno], tablero: tablero
+    turno: jugadores[turno], tablero: tablero, ganador: ganador
   });
 
 });
--- test/tateti.js
+++ test/tateti.js
@@ -6,7 +6,7 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-//Cuando se inicia un juego nuevo le toca al primer jugador y el tablero esta vacio
+//Cuando se inicia un juego nuevo le toca al primer jugador y el tablero esta vacio y el nombre del ganador es ninguno
 //Cuando el primer jugador hace su movimiento le toca al otro jugador y la casilla elegida por el primer jugador esta ocupada
 //Cuando el segundo jugador hace su movimiento entonces le toca de nuevo al primer jugador y las casillas elegida por el primer y segundo jugador esta ocupadas con marcas diferentes
 //Cuando un jugador marca tres casillas de la misma fila entonces gana
@@ -14,6 +14,8 @@ chai.use(chaiHttp);
 //Cuando un jugador marca tres casillas de la misma diagonal entonces gana
 //Si un jugador mueve cuando no es su turno entonces se devuelve un error y el tablero no se modifica
 //Cuando no quedan casilla vacias y no hay un ganador entonces es un empate
+//Cuando el segundo jugador gana
+
 describe("juego tateti", () => {
 
     let juego = {
@@ -28,8 +30,62 @@ describe("juego tateti", () => {
         { jugador: 'Juan', columna: 0, fila: 2 }
     ]
 
+    let secuenciafila = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 1 },
+        { jugador: 'Juan', columna: 1, fila: 0 },
+        { jugador: 'Pedro', columna: 2, fila: 1 },
+
+    ]
+
+    let secuenciacolumna = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 0, fila: 1 },
+        { jugador: 'Pedro', columna: 1, fila: 1 }
+
+    ]
+
+    let secuenciadiagonal = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 1, fila: 1 },
+        { jugador: 'Pedro', columna: 2, fila: 1 }
+
+    ]
+
+    let secuenciafallaturno = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 1, fila: 1 },
+    ]
+
+    let secuenciaempate = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 1, fila: 1 },
+        { jugador: 'Pedro', columna: 2, fila: 2 },
+        { jugador: 'Juan', columna: 2, fila: 0 },
+        { jugador: 'Pedro', columna: 2, fila: 1 },
+        { jugador: 'Juan', columna: 0, fila: 2 },
+        { jugador: 'Pedro', columna: 0, fila: 1 }
+
+
+    ]
+
+    let segunciasegundo = [
+        { jugador: 'Juan', columna: 2, fila: 2 },
+        { jugador: 'Pedro', columna: 0, fila: 0 },
+        { jugador: 'Juan', columna: 1, fila: 1 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 2, fila: 1 }
+
+    ]
+
+
+
     describe("Se empieza un juego nuevo", () => {
-        it("Todos los casilleros estan vacios y Le toca al primer jugador", (done) => {
+        it("Todos los casilleros estan vacios y Le toca al primer jugador y el nombre del ganador es ninguno", (done) => {
             chai.request(server)
                 .put("/empezar")
                 .send(juego)
@@ -38,6 +94,7 @@ describe("juego tateti", () => {
                     res.should.to.be.json;
                     res.body.should.be.a('object');
                     res.body.should.have.property('turno').eql('Juan');
+                    res.body.should.have.property('ganador').eql('ninguno');
                     res.body.should.have.property('tablero').eql([
                         [' ', ' ', ' '],
                         [' ', ' ', ' '],
@@ -85,7 +142,7 @@ describe("juego tateti", () => {
                 .put("/movimiento")
                 .send(movimientos[0])
                 .end();
-                
+
             chai.request(server)
                 .put("/movimiento")
                 .send(movimientos[1])
@@ -101,10 +158,200 @@ describe("juego tateti", () => {
                     ]);
                     done();
                 });
-
         });
+    });
+    describe("Cuando un jugador marca tres casillas de la misma fila entonces gana", () => {
 
+        it("el jugador Juan marca una fila y gana el juego", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
 
+            secuenciafila.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Juan', columna: 2, fila: 0 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ganador').eql('Juan');
+                    res.body.should.have.property('tablero').eql([
+                        ['X', 'X', 'X'],
+                        [' ', 'O', 'O'],
+                        [' ', ' ', ' ']
+                    ]);
+                    done();
+                });
+        });
+    });
+    describe("Cuando un jugador marca tres casillas de la misma columna entonces gana", () => {
+
+        it("el jugador Juan marca una columna y gana el juego", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
+
+            secuenciacolumna.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Juan', columna: 0, fila: 2 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ganador').eql('Juan');
+
+                    done();
+                });
+        });
+    });
+
+    describe("Cuando un jugador marca tres casillas de la misma diagonal entonces gana", () => {
+
+        it("el jugador Juan marca una diagonal y gana el juego", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
+
+            secuenciadiagonal.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Juan', columna: 2, fila: 2 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ganador').eql('Juan');
+                    done();
+                });
+        });
+    });
+
+    describe("Si un jugador mueve cuando no es su turno entonces se devuelve un error y el tablero no se modifica", () => {
+
+        it("el jugador Juan mueve dos veces seguidas y causa un error el tablero no se modifica", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
+
+            secuenciafallaturno.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Juan', columna: 0, fila: 2 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('tablero').eql([
+                        ['X', 'O', ' '],
+                        [' ', 'X', ' '],
+                        [' ', ' ', ' ']
+                    ]);
+
+                    done();
+                });
+        });
+    });
+
+    describe("Cuando no quedan casilla vacias y no hay un ganador entonces es un empate", () => {
+
+        it("el tablero se llena sin ningun ganador", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
+
+            secuenciaempate.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Juan', columna: 1, fila: 2 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('tablero').eql([
+                        ['X', 'O', 'X'],
+                        ['O', 'X', 'O'],
+                        ['X', 'X', 'O']
+                    ]);
+                    res.body.should.have.property('ganador').eql('empate');
+
+                    done();
+                });
+        });
+    });
+
+    describe("Cuando el segundo jugador gana", () => {
+
+        it("el jugador Juan completa una fila y gana", (done) => {
+            chai.request(server)
+                .put("/empezar")
+                .send(juego)
+                .end();
+
+            segunciasegundo.forEach(mov => {
+
+                chai.request(server)
+                    .put("/movimiento")
+                    .send(mov)
+                    .end();
+            });
+
+            chai.request(server)
+                .put("/movimiento")
+                .send({ jugador: 'Pedro', columna: 2, fila: 0 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('ganador').eql('Pedro');
+
+                    done();
+                });
+        });
     });
 
 });
+
+
+
